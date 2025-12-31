75f1c0da9ea707f71ff1a61b5eeda2996434a2ca
Jaemin Choi <gemini6c@gmail.com>
2025-12-31 14:23:48 +0900

    251213

--- missions/W1/m3/etl_project_gdp.py
+++ missions/W1/m3/etl_project_gdp.py
@@ -16,10 +16,11 @@ def main(user_agent: str, save_options: list[str]) -> None:
         log('[INFO] Process started: GDP Data ETL')
         gdp_table = extract_gdp_from_wiki(user_agent)
         df = transfrom_data(gdp_table)
-        load_data(df, save_options)
+        country_region_map = get_country_region_map()
+        load_data(df, save_options, country_region_map)
         log('[INFO] Process completed succesfully')
     except Exception as e:
-        log('[ERROR] Pipeline stopped')
+        log(f'[ERROR] Pipeline stopped: {e}')
     
 
 def extract_gdp_from_wiki(user_agent: str) -> list:
@@ -27,7 +28,7 @@ def extract_gdp_from_wiki(user_agent: str) -> list:
     header = {'User-Agent': user_agent}
     html_content = fetch_gdp_data(url, header)
 
-    # html 에서 gdp 테이블 부분 검색하여 추출
+    # html 에서 gdp 테이블 부분 검색하여 추출 / 테이블의 각 행을 리스트에 담아 리턴
     soup = BeautifulSoup(html_content,'html.parser')
     gdp_table = soup.find('table', class_='wikitable')
     if not gdp_table:
@@ -59,17 +60,25 @@ def transfrom_data(rows: list) -> pd.DataFrame:
 
 
 
-def load_data(df: pd.DataFrame, save_options: list[str]) -> None:
+def load_data(df: pd.DataFrame, save_options: list[str], country_region_map: dict) -> None:
     ''' print and save GDP data '''
-    # 테이블의 1열(국가정보), 2열(최신 gdp 현황)만 사용
+    # 테이블의 1열(국가정보), 2열(최신 IMF gdp 현황)만 사용
     df = df.iloc[1:,0:2]
     df.iloc[:,1] = df.iloc[:,1].apply(convert_unit_to_B)
-    # 최신 gdp 현황의 값이 NA인 국가 제거
-    df.dropna()
+    # IMF gdp 현황의 값이 NA인 국가 제거
+    df = df.dropna()
     table_name = df.columns[-1]
     df.columns = ['Country','GDP_USD_billion']
+
+    #GDP가 100B USD이상이 되는 국가만을 구해서 화면에 출력
     filtered_df = df[df.iloc[:,1] >= 100]
     print(filtered_df)
+    #각 Region별로 top5 국가의 GDP 평균을 구해서 화면에 출력
+    region_added_df = df.assign(Region = lambda x: x['Country'].map(country_region_map))
+    region_added_df.dropna()
+    region_added_df['GDP_USD_billion'] = pd.to_numeric(region_added_df['GDP_USD_billion'])
+    top5_Average = region_added_df.groupby('Region')['GDP_USD_billion'].apply(lambda x: x.nlargest(5).mean())
+    print(top5_Average)
 
     if 'json' in save_options:
         save_json(df)
@@ -101,6 +110,10 @@ def convert_unit_to_B(money_str: str) -> float:
         return None
     return round(float(converted_str)/1000, 2)
 
+def get_country_region_map() -> dict[str:str]:
+    df = pd.read_csv('country_data.tsv', sep='\t')
+    result= df.set_index('name_short')['Continent_7'].to_dict()
+    return result
 
 if __name__ == "__main__":
     main(user_agent='Chrome/143.0.0.0', save_options=['json', 'db'])
